69ea468a0a1696577a25522db65d19b5062865ca
Rodrigo Cisternas <rodcisal@gmail.com>
2012-11-28 00:21:36 -0300

    friendly_id and set proper image size

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ end
 gem 'jquery-rails'
 gem 'twitter-bootstrap-rails'
 gem 'devise'
+gem 'friendly_id'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/pelicula.rb
+++ app/models/pelicula.rb
@@ -1,4 +1,6 @@
 class Pelicula < ActiveRecord::Base
+	extend FriendlyId
 	belongs_to :uploader
   attr_accessible :descripcion, :iframe, :imagen, :titulo, :uploader_id
+  friendly_id :titulo
 end
