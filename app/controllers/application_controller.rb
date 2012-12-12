94cca8af69e51d617f8537d5c2e7a8ea43e939a9
Rodrigo Cisternas <rodcisal@gmail.com>
2012-12-12 00:18:26 -0300

    mejorando el css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :footers 
+ def footers
+   Pelicula.find(:all, :order => 'created_at DESC', :limit => 5)
+ end
+
 end
